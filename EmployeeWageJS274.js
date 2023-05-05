158e57de41db392e27e86e5b3ab0dd4b5ee8ac07
shivrjpatil <shivrajp972@gmail.com>
2023-05-05 20:24:58 +0530

    Added Program to caculate employee wage

--- EmployeeWageJS274.js
+++ EmployeeWageJS274.js
@@ -6,3 +6,27 @@ if(empcheck==IsPresent)
 console.log("Employee is present")
 else
 console.log("Employee is absent")
+
+//UC2-Calculate Employee wage Considering the factors employee is full time or Parttime
+console.log("UC2 OUTPUT");
+const FullTimeHour=8;
+const PartTimeHour=4;
+const IsFullTime=1;
+const IsPartTime=2;
+const WagePerHour=20;
+
+let emphrs=0;
+let empcheck1=Math.floor(Math.random()*10%2);
+switch(empcheck1){
+    case IsFullTime:
+        emphrs=FullTimeHour;
+        break;
+    case IsPartTime:
+         emphrs=PartTimeHour;
+         break;
+    default:
+         emphrs=0;
+         break;
+}
+let empwage=emphrs*WagePerHour;
+console.log("Total wage:"+empwage);
