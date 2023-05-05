43f46f3bbf132c1e9523a592da23d4228352accd
shivrjpatil <shivrajp972@gmail.com>
2023-05-05 20:20:26 +0530

    Added program to check Employee Attendance

---
+++ EmployeeWageJS274.js
@@ -0,0 +1,8 @@
+//UC1- to check weather employee is present or absent
+console.log("UC1 OUTPUT");
+const IsPresent=1;
+let empcheck=Math.floor(Math.random()*10%2)
+if(empcheck==IsPresent)
+console.log("Employee is present")
+else
+console.log("Employee is absent")
